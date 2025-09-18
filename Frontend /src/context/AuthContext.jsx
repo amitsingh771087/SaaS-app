0a96b7cb146da5fff0395a5cc998add9d85ba2e8
amitsingh771087 <amitsingh77108792@gmail.com>
2025-09-18 22:05:37 +0530

    checking and verify token

--- Frontend /src/context/AuthContext.jsx
+++ Frontend /src/context/AuthContext.jsx
@@ -38,13 +38,55 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const register = async (username, email, password) => {
+    try {
+      const res = await axiosClient.post("signup/", {
+        username,
+        email,
+        password,
+      });
+
+      if (res.status === 201) {
+        localStorage.setItem("user", JSON.stringify({ username, email }));
+        setUser({ username, email });
+        return { success: true, message: "Registration successful!" };
+      }
+    } catch (error) {
+      if (error.response?.data?.error === "User already exists") {
+        throw new Error("User already exists");
+      }
+      throw new Error("Registration failed. Please try again.");
+    }
+  };
+
+  const verifyToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return false;
+
+    try {
+      await axiosClient.post("token/verify/", { token });
+      return true;
+    } catch (err) {
+      logout();
+      return false;
+    }
+  };
+
   const logout = () => {
     localStorage.removeItem("token");
     setUser(null);
   };
 
+  useEffect(() => {
+    (async () => {
+      await verifyToken();
+    })();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ user, login, logout, isAuth: !!user }}>
+    <AuthContext.Provider
+      value={{ user, login, register, logout, isAuth: !!user }}
+    >
       {children}
     </AuthContext.Provider>
   );
