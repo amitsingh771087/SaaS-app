f0ea70cdd4f8ccf4216b1eee701b90f2b27fe8da
amitsingh771087 <amitsingh77108792@gmail.com>
2025-09-18 16:23:14 +0530

    added verify token

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
-    TokenRefreshView)
+    TokenRefreshView,TokenVerifyView,)
 from .views import( ItemViewSet, UserViewSet, TenantViewSet, TenantUserViewSet, CustomerViewSet,ItemCategoryViewSet,ItemPriceViewSet,CustomerListCreateView,
     CustomerDetailView,
     CustomerTimelineView,
@@ -34,6 +34,7 @@ urlpatterns = [
     # JWT Auth Endpoints
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     
     # customers 
     path("customers/", CustomerListCreateView.as_view(), name="customer-list-create"),
